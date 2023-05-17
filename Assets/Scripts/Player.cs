d6e026968a9cbab35e1619dd2fd47fa90daf6ff7
KuroKumo <loicjacob.pro@gmail.com>
2023-05-17 17:34:55 +0900

    Add special weapon

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,6 +14,8 @@ public class Player : MonoBehaviour
     [Header ("Settings")]
     [SerializeField] private float moveSpeed = 5;
     [SerializeField] private float rotationSpeed = 5;
+    [SerializeField] private float pressChangeWeaponTimeToHold = 0.5f;
+    [SerializeField] private float pressReloadTimeToHold = 0.5f;
 
     private PlayerInput playerInput;
     private InputAction moveAction;
@@ -28,14 +30,21 @@ public class Player : MonoBehaviour
     private InputAction weaponAbilityAction;
 
     private Gun currentGun;
+    private Gun lastBasicGunUsed;
 
     private Vector3 moveDirection;
     private Vector3 aimDirection;
 
     private Quaternion camForwardOn2DPlane;
 
-    private bool isFiring = false;
-    private bool isHoldingOneHandedGun = true;
+    private bool isPressingFire = false;
+    private bool isPressingChangeWeapon = false;
+    private bool isPressingReload = false;
+    private bool isHoldingChangeWeapon = false;
+    private bool isHoldingReload = false;
+
+    private float pressChangeWeaponElapsedTime = 0;
+    private float pressReloadElapsedTime = 0;
 
     private void Awake()
     {
@@ -58,8 +67,10 @@ public class Player : MonoBehaviour
         throwAction.performed += Throw;
         knifeAction.performed += Knife;
         lampAction.performed += Lamp;
-        reloadAction.performed += Reload;
-        changeWeaponAction.performed += ChangeWeapon;
+        reloadAction.performed += OnPerformReload;
+        reloadAction.canceled += OnCancelReload;
+        changeWeaponAction.performed += OnPerformChangeWeapon;
+        changeWeaponAction.canceled += OnCancelChangeWeapon;
         changeThrowAction.performed += ChangeThrow;
         weaponAbilityAction.performed += WeaponAbility;
     }
@@ -67,24 +78,15 @@ public class Player : MonoBehaviour
     private void Start()
     {
         currentGun = oneHandedGunSlot.GetComponentInChildren<Gun>();
-    }
-
-    private void OnPerformFire(InputAction.CallbackContext context)
-    {
-        isFiring = true;
-    }
-
-    private void OnCancelFire(InputAction.CallbackContext context)
-    {
-        isFiring = false;
+        oneHandedGunSlot.SetActive(true);
+        twoHandedGunSlot.SetActive(false);
+        specialGunSlot.SetActive(false);
+        lastBasicGunUsed = oneHandedGunSlot.activeSelf ? oneHandedGunSlot.GetComponentInChildren<Gun>() : twoHandedGunSlot.GetComponentInChildren<Gun>();
     }
 
     private void Update()
     {
         GetInput();
-
-        if (isFiring)
-            Fire();
     }
 
     private void FixedUpdate()
@@ -93,11 +95,102 @@ public class Player : MonoBehaviour
         Aim();
     }
 
+    #region Get Inputs
     private void GetInput()
     {
         moveDirection = new Vector3(moveAction.ReadValue<Vector2>().x, 0, moveAction.ReadValue<Vector2>().y);
         aimDirection = new Vector3(aimAction.ReadValue<Vector2>().x, 0, aimAction.ReadValue<Vector2>().y);
+
+        if (isPressingFire)
+            OnPressFire();
+
+        if (isPressingChangeWeapon)
+            OnPressChangeWeapon();
+
+        if (isPressingReload)
+            OnPressReload();
     }
+
+    private void OnPerformFire(InputAction.CallbackContext context)
+    {
+        isPressingFire = true;
+    }
+
+    private void OnPressFire()
+    {
+        currentGun.Shoot();
+    }
+
+    private void OnCancelFire(InputAction.CallbackContext context)
+    {
+        isPressingFire = false;
+    }
+
+    private void OnPerformChangeWeapon(InputAction.CallbackContext context)
+    {
+        isPressingChangeWeapon = true;
+        isHoldingChangeWeapon = false;
+        pressChangeWeaponElapsedTime = 0;
+    }
+
+    private void OnPressChangeWeapon()
+    {
+        pressChangeWeaponElapsedTime += Time.deltaTime;
+
+        if (pressChangeWeaponElapsedTime >= pressChangeWeaponTimeToHold && !isHoldingChangeWeapon)
+        {
+            isHoldingChangeWeapon = true;
+
+            lastBasicGunUsed.transform.parent.gameObject.SetActive(!lastBasicGunUsed.transform.parent.gameObject.activeSelf);
+            specialGunSlot.SetActive(!specialGunSlot.activeSelf);
+            currentGun = specialGunSlot.activeSelf ? specialGunSlot.GetComponentInChildren<Gun>() : lastBasicGunUsed;
+        }
+    }
+
+    private void OnCancelChangeWeapon(InputAction.CallbackContext context)
+    {
+        isPressingChangeWeapon = false;
+
+        if (!isHoldingChangeWeapon)
+        {
+            GameObject previousGunSlot = currentGun.transform.parent.gameObject;
+
+            previousGunSlot.SetActive(false);
+            currentGun = previousGunSlot == specialGunSlot ? 
+                lastBasicGunUsed : previousGunSlot == oneHandedGunSlot ? 
+                twoHandedGunSlot.GetComponentInChildren<Gun>() : oneHandedGunSlot.GetComponentInChildren<Gun>();
+
+            currentGun.transform.parent.gameObject.SetActive(true);
+            lastBasicGunUsed = currentGun;
+        }
+    }
+
+    private void OnPerformReload(InputAction.CallbackContext context)
+    {
+        isPressingReload = true;
+        isHoldingReload = false;
+        pressReloadElapsedTime = 0;
+    }
+
+    private void OnPressReload()
+    {
+        pressReloadElapsedTime += Time.deltaTime;
+
+        if (pressReloadElapsedTime >= pressReloadTimeToHold && !isHoldingReload)
+        {
+            isHoldingReload = true;
+            Debug.Log("Open reload radial menu");
+        }
+    }
+
+    private void OnCancelReload(InputAction.CallbackContext context)
+    {
+        isPressingReload = false;
+
+        if (!isHoldingReload)
+            currentGun.Reload();
+    }
+    #endregion
 
     private void Move()
     {
@@ -113,19 +206,9 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void Fire()
-    {
-        currentGun.Shoot();
-    }
-
     private void Throw(InputAction.CallbackContext context)
     {
 
-    }
-
-    private void Reload(InputAction.CallbackContext context)
-    {
-        currentGun.Reload();
     }
 
     private void Knife(InputAction.CallbackContext context)
@@ -141,16 +224,6 @@ public class Player : MonoBehaviour
     private void WeaponAbility(InputAction.CallbackContext context)
     {
 
-    }
-
-    private void ChangeWeapon(InputAction.CallbackContext context)
-    {
-        isHoldingOneHandedGun = !isHoldingOneHandedGun;
-
-        currentGun.transform.parent.gameObject.SetActive(false);
-        currentGun = isHoldingOneHandedGun ? oneHandedGunSlot.GetComponentInChildren<Gun>() : twoHandedGunSlot.GetComponentInChildren<Gun>();
-        currentGun.transform.parent.gameObject.SetActive(true);
-        
     }
 
     private void ChangeThrow(InputAction.CallbackContext context)
